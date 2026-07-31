969243550b0b8ec28f36b0c8800f12d26576d641
mdalaf213-create <mdalaf213@gmail.com>
2026-07-31 17:15:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from google.protobuf.timestamp_pb2 import Timestamp
 # আপনার কাস্টম মডিউল (একই ফোল্ডারে থাকতে হবে)
 from byte import *
 from byte import xSEndMsg, Auth_Chat
-from xHeaders import 
+from xHeaders import *
 from black9 import openroom, spmroom
 import xKEys
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
