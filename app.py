c2397c217638b7e84f2c19c5939a7bf9c4c22d46
mdalaf213-create <mdalaf213@gmail.com>
2026-07-31 17:22:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from Crypto.Util.Padding import pad, unpad
 from google.protobuf.timestamp_pb2 import Timestamp
 # আপনার কাস্টম মডিউল (একই ফোল্ডারে থাকতে হবে)
 from byte import *
-from byte import xSEndMsg, Auth_Chat *
 from xHeaders import *
 from black9 import openroom, spmroom
 import xKEys
